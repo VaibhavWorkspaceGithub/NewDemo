266073a26fbc331580f07c7890e532f63c2b4231
VGK <vaibhavkasar5896@gmail.com>
2023-01-18 18:52:08 +0530

    E commerce project

---
+++ MiniProject E-commerce/src/com/test/MainTest.java
@@ -0,0 +1,20 @@
+package com.test;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+
+//		ConnectionNew cnw = new ConnectionNew();
+//
+//		cnw.getConnection();
+//
+	Products p = new Products();
+//
+//		p.addProductDetails();
+		Register r = new Register();
+
+		r.registerUser();
+		p.getProducts();
+	}
+
+}
